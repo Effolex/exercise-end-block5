6bcd3eaffa0b18cc77378306f2a60a82258d232e
Eric Cruz <effolex@gmail.com>
2021-08-12 18:31:10 -0300

    resolucao da atividade em grupo

--- script.js
+++ script.js
@@ -1,56 +1,66 @@
-window.onload = function() {
-  let basePyramid = 9;
-  let numberOfLines = (basePyramid + 1) / 2; // 5
-  let controlLeft = numberOfLines; // 5
-  let controlRight = numberOfLines; // 5
-  let lines = document.querySelectorAll(".line");
 
-  updateVisit();
+let basePyramid = 9;
+let numberOfLines = (basePyramid + 1) / 2; // 5
+let controlLeft = numberOfLines; // 5
+let controlRight = numberOfLines; // 5
 
-  fillTriangle(lines);
+function criaDiv(numberOfLines){
+  for(let i = 0; i< numberOfLines; i++){
+    let div = document.createElement('div');
+    div.classList.add('line');
+    document.querySelector('.triangle').appendChild(div);
+  }
   
-  // Atualiza a quantidade de visitar no site, utilizando o LocalStorage
-  function updateVisit() {
-    if (typeof (Storage) != "undefined") {
-      if(localStorage.count !== undefined) {
-        let count = parseInt(localStorage.count);
-        count+=1;
-        localStorage.count = count;
-        document.getElementById("count").innerHTML = count;
-      } else {
-        localStorage.count = 1;
-        document.getElementById("count").innerHTML = 1;
-      }
+}
+
+criaDiv(numberOfLines);
+let lines = document.querySelectorAll(".line");
+
+updateVisit();
+
+fillTriangle(lines);
+
+// Atualiza a quantidade de visitar no site, utilizando o LocalStorage
+function updateVisit() {
+  if (typeof (Storage) != "undefined") {
+    if(localStorage.count !== undefined) {
+      let count = parseInt(localStorage.count);
+      count+=1;
+      localStorage.count = count;
+      document.getElementById("count").innerHTML = count;
     } else {
-      document.write("Sem suporte para Web Storage");
-    }  
-  }
-
-  // Passa por todos as linhas (div com class line) e preenche o triangulo
-  function fillTriangle(lines) {
-    for(let index = 0; index < lines.length; index += 1) {
-      fillLine(lines[index]);
-      controlRight += 1;
-      controlLeft -= 1;
+      localStorage.count = 1;
+      document.getElementById("count").innerHTML = 1;
     }
-  }
+  } else {
+    document.write("Sem suporte para Web Storage");
+  }  
+}
 
-  // Cria uma caixa com base nas diferentes classes
-  function createBox(className) {
-    let box = document.createElement("div");
-    box.className = className;
-    return box;
+// Passa por todos as linhas (div com class line) e preenche o triangulo
+function fillTriangle(lines) {
+  for(let index = 0; index < lines.length; index += 1) { //+1
+    fillLine(lines[index]);
+    controlRight += 1;
+    controlLeft -= 1;
   }
+}
 
-  // Preenche uma linha
-  function fillLine(divLine) {
-    for (let lineColumn = 1; lineColumn <= basePyramid; lineColumn += 1) {
-      if(lineColumn >= controlLeft && lineColumn <= controlRight) {
-        let box = createBox("box");
-        divLine.appendChild(box);
-      } else {
-        divLine.appendChild(createBox("box-empty"));
-      }
+// Cria uma caixa com base nas diferentes classes
+function createBox(className) {
+  let box = document.createElement("div");
+  box.className = className;
+  return box;
+}
+
+// Preenche uma linha
+function fillLine(divLine) {
+  for (let lineColumn = 1; lineColumn <= basePyramid; lineColumn += 1){//+1 
+    if(lineColumn >= controlLeft && lineColumn <= controlRight) {//+3
+      divLine.appendChild(createBox("box"));
+    } else { //+4
+      divLine.appendChild(createBox("box-empty"));
     }
   }
 }
+
